4fc05588bd65a2a5f26975175b4994d9855987c0
luciano silva da costa maninho <luciano.silvadacosta@outlook.com>
2019-04-04 20:57:30 -0400

    https://github.com/Lucianosilvadacosta/Aula04_04.git

---
+++ VIEWS_TIPADAS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VIEWS_TIPADAS.MvcApplication" Language="C#" %>
---
+++ VIEWS_TIPADAS/Models/FUNCAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VIEWS_TIPADAS.Models
+{
+    public class FUNCAO
+    {
+        public int FuncaoId { get; set; }
+        public string NomeFuncao { get; set; }
+        public decimal Salario { get; set; }
+
+    }
+}
